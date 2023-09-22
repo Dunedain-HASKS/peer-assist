c772e92816b44436544dd293d77ef0bd3a014560
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-23 05:26:14 +0530

    set up profile fetch function

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,22 @@
+"use client";
 
+import { useAuth } from "@/context/session";
+import { User } from "@/types/user.interface";
+import { useState, useEffect } from "react";
+import { fetchProfile } from "./action";
 
 export default function Page() {
+    const { session } = useAuth();
+    const [profile, setProfile] = useState({});
+    useEffect(() => {
+        fetchProfile(session).then(({ user }) => {
+            setProfile(user);
+        });
+    }, [session]);
     return (
         <>
             <h1>User</h1>
+            <p>{JSON.stringify(profile)}</p>
         </>
     );
 }
