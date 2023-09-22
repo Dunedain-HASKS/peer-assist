f045dd6e611adf959b2ac5b32d123e03bb45283d
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-09-23 01:01:03 +0530

    create and add question card

---
+++ src/components/questionCard.tsx
@@ -0,0 +1,31 @@
+"user client"
+
+import React from 'react';
+import { Container, Typography } from "@mui/material";
+
+interface QuestionCardProps {
+  id: {
+    user: string;
+    question: string;
+    answers: Record<string, string>;
+  };
+}
+
+const QuestionCard: React.FC<QuestionCardProps> = ({ id }) => {
+  const { user, question, answers } = id;
+
+  return (
+    <Container sx={{border:'1px solid white', my:3, borderRadius:2, bgcolor:'#8789C0', color:'#12273f'}}>
+      <Typography variant="h6" sx={{py:1}}>{user}</Typography>
+      <Typography variant="h5">{question}</Typography>
+      {/* <Typography variant="h6">Answers:</Typography> */}
+      <ul style={{paddingBottom:5}}>
+        {Object.values(answers).map((answer, answerIndex) => (
+          <li key={answerIndex}>{answer}</li>
+        ))}
+      </ul>
+    </Container>
+  );
+};
+
+export default QuestionCard;
