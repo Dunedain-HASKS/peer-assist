52a16c8fce5873c3edaf52e7c50b8445b9d80952
Anuj Contractor <63806427+anujcontractor@users.noreply.github.com>
2023-09-23 02:56:19 +0530

    create org profile

--- src/app/organization/page.tsx
+++ src/app/organization/page.tsx
@@ -1,11 +1,54 @@
 "use client";
 
-// pages/organization/[organizationId].tsx
+import { Container, Typography } from "@mui/material";
+
+const data = [
+    {
+        users: ['user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7', 'user8', 'user9', 'user10'],
+        name: 'Organization A',
+        description: 'This is Organization A',
+        domain: 'orga.com',
+      },
+      {
+        users: ['user4', 'user5'],
+        name: 'Organization B',
+        description: 'This is Organization B',
+        domain: 'orgb.com',
+      },
+      {
+        users: ['user6'],
+        name: 'Organization C',
+        description: 'This is Organization C',
+        domain: 'orgc.com',
+      },
+      {
+        users: ['user7', 'user8', 'user9'],
+        name: 'Organization D',
+        description: 'This is Organization D',
+        domain: 'orgd.com',
+      },
+      {
+        users: ['user10'],
+        name: 'Organization E',
+        description: 'This is Organization E',
+        domain: 'orge.com',
+      },
+];
 
 export default function Page() {
     return (
         <>
-            <h1>Organization</h1>
+            <Typography variant="h2" sx={{ py: 1 , color: "red"}}>Name: {data[0].name}</Typography>
+            <Typography variant="h4" sx={{ py: 1 , color: "red"}}>Domain: {data[0].domain}</Typography>
+            <Typography variant="h5" sx={{ py: 1 , color: "red"}}>Description: {data[0].description}</Typography>
+            <ul style={{ paddingBottom: 5, fontSize: '1.5rem' }}>
+                {data[0].users.map((user, userIndex) => (
+                    <li key={userIndex}>
+                        <Typography variant="subtitle1" sx={{ color: '#ccc' }}>{user}:</Typography>
+                    </li>
+                ))}
+            </ul>
+
         </>
   );
 }
