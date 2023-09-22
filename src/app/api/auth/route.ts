2bd5f46efb570f44c854de678ae6eddb87f9806b
Harshal Patel <harshal5820@gmail.com>
2023-09-23 03:31:35 +0530

    verify access tokens

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,8 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-import { createTokenWithUserName, createTokenWithEmail } from "@/server/services/auth.service";
+import { createTokenWithUserName, createTokenWithEmail, verifyToken } from "@/server/services/auth.service";
+
+// export async function POST(request: NextRequest, context: any) {
+//     const { user } = await request.json();
+//     const newToken = await createTokenWithEmail({ email: user.email, password: user.password });
+//     return NextResponse.json({ newToken }, { status: 200 });
+// };
 
 export async function POST(request: NextRequest, context: any) {
-    const { user } = await request.json();
-    const newToken = await createTokenWithEmail({ email: user.email, password: user.password });
+    const { token } = await request.json();
+    const newToken = await verifyToken({ token: token.token});
     return NextResponse.json({ newToken }, { status: 200 });
 };
--- src/server/services/auth.service.ts
+++ src/server/services/auth.service.ts
@@ -1,42 +1,44 @@
-import { sign } from "jsonwebtoken";
+import { sign, verify } from "jsonwebtoken";
 import { UserModel } from "../models/user.model";
 import bcrypt from "bcrypt";
 
 const secret = process.env.JWT_SECRET;
 
 export const verifyToken = ({ token }: { token: string }) => {
-    // 
+    if(secret === undefined) throw new Error("JWT_SECRET is not defined");
+    const id = verify(token, secret, (err, decoded) => {
+        if(err) throw new Error("Token is invalid");
+        return decoded;
+    });
+    return id;
 };
 
 export const createTokenWithUserName = async ({ username, password }: { username: string, password: string }) => {
     if(secret === undefined) throw new Error("JWT_SECRET is not defined");
 
-    await UserModel.findOne({ username }).then((user) => {
+    const{ id } = await UserModel.findOne({ username }).then((user) => {
         if(user === null) throw new Error("User not found");
         if(!bcrypt.compare(password, user.password)) throw new Error("Password is incorrect");
+        return user;
     });
 
-    const access_token = sign({ username }, secret, { expiresIn: "12h" });
+    const access_token = sign({ id }, secret, { expiresIn: "12h" });
     return access_token;
 };
 
 export const createTokenWithEmail = async ({ email, password }: { email: string, password: string }) => {
     if(secret === undefined) throw new Error("JWT_SECRET is not defined");
 
-    const user = await UserModel.findOne({ email }).then((user) => {
+    const { id } = await UserModel.findOne({ email }).then((user) => {
         if(user === null) throw new Error("User not found");
         if(!bcrypt.compare(password, user.password)) throw new Error("Password is incorrect");
         else return user;
     });
 
-    const access_token = sign({ username: user.username }, secret, { expiresIn: "12h" });
+    const access_token = sign({ id }, secret, { expiresIn: "12h" });
     return access_token;
 };
 
 export const invalidateToken = ({ token }: { token: string }) => {
-    // 
+    //
 };
-
-
-
-
