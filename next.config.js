e5bbcbe3914451cef061d4d8a13799a7ab02481c
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-23 01:44:11 +0530

    add instrumentation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+        instrumentationHook: true,
+    }
+}
 
 module.exports = nextConfig
