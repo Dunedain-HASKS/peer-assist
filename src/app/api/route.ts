2095236c0ce33044c6e8d7fc566157d20aaee824
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-23 23:59:01 +0530

    remove routes

--- src/app/api/route.ts
+++
@@ -1,6 +0,0 @@
-import { generateData } from "@/server/seeds/seed";
-import { NextRequest, NextResponse } from "next/server";
-export async function POST(request: NextRequest, context: any) {
-    await generateData();
-    return NextResponse.json({ message: "Data generated" }, { status: 200, statusText: "OK"});
-}; 
