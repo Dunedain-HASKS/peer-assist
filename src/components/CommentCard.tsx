07ddac825fe86455b4c64f1f63ce27d9d81a22ef
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-09-23 16:01:07 +0530

    add comment card to question page

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -1,29 +1,26 @@
 "use client";
 
 import React from 'react';
-import { Container, Typography } from "@mui/material";
-// import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-// import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import { Container, Typography, Divider } from "@mui/material";
 
-const CommentCard = ({ id }: { id: string}) => {
-    const content = "Fook Linda";
+const CommentCard = ({ id }: { id: string }) => {
+    const content = "Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda Fook Linda ";
     const user = "User";
-    return (
-        <Container sx={{ color: '#22b', bgcolor: 'yellow', my: 3, borderRadius: 2,
-        boxShadow: 3, padding: "10px"
-        
-        }}>
-            <div style={{ display: 'flex' }}>
-                <div style={{display: "flex", padding: "10px"}}>
-                <Typography variant="h5" sx={{  }}>{content}</Typography>
-                <Typography variant="h5" sx={{ ml:4 }}>{user}</Typography>
-                </div>
-            </div>
 
-            {/* <Typography variant='h6' sx={{ display: 'flex', justifyContent: 'flex-end', padding: "10px" }}>
-                {user}
-            </Typography> */}
-        </Container>
+    return (
+        <div style={{
+            width: '100%',
+            color: '#22b',
+            // my: 3,
+            borderRadius: 2,
+            padding: "10px",
+        }}>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: "10px" }}>
+                <Typography variant="h6" sx={{ flex: 1 }}>{content}</Typography>
+                <Typography variant="h6" sx={{ ml: 4 }}>{user}</Typography>
+            </div>
+            <Divider/> {/* Add a divider */}
+        </div>
     );
 };
 
