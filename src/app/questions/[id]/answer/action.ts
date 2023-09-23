7e81d99ad2096e81eb80125cd57f110bdc78619e
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-23 16:29:36 +0530

    rename func

--- src/app/questions/[id]/answer/action.ts
+++ src/app/questions/[id]/answer/action.ts
@@ -5,13 +5,14 @@ import { postAnswer } from "@/server/services/answer.service";
 import { verifyToken } from "@/server/services/auth.service";
 import { AnswerInput } from "@/types/answer.interface";
 
-export async function postAnswerActions({ answer_input, session, questionId }: {
+export async function postAnswerAction({ answer_input, session, questionId }: {
     answer_input: AnswerInput,
     session: SessionInterface,
     questionId: string,
 }) {
     const { id } = verifyToken(session);
     const answer = await postAnswer({ answer_input, userId: id, questionId: questionId });
+    console.log(answer);
     return answer;
 };
 
