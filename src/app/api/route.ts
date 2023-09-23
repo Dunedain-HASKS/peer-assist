fd7bfaa86b7d774a4c9c03207dbe3b6ca72e9e82
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-23 14:59:42 +0530

    remove tests

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-import { closeQuestion, getQuestionThread, postCommentToQuestion, postQuestion, voteQuestion } from "@/server/services/question.service";
-
 export async function POST(request: NextRequest, context: any) {
-    const { question, userId } = await request.json();
-    await postQuestion({ question_input: question, userId });
-}; 
+    return NextResponse.redirect("/");
+} ; 
