81779d2f8ba7e2455e52c04cc16f53674e891d3e
Harshal Patel <harshal5820@gmail.com>
2023-09-23 06:32:30 +0530

    create comment service

--- src/app/api/ans/route.ts
+++ src/app/api/ans/route.ts
@@ -1,8 +1,9 @@
-import { createAnswer, getAnswer, postComment, updateAnswer, voteAnswer } from "@/server/services/answer.service";
+import { createAnswer, deleteAnswer, getAnswer, postComment, updateAnswer, voteAnswer } from "@/server/services/answer.service";
+import { deleteComment, getComment } from "@/server/services/comment.service";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest, context: any) {
     const { answer } = await request.json();
-    const newAnswer = await postComment({ answerId: answer.answerId, comment_input: answer.comment_input, userId: answer.userId });
+    const newAnswer = await deleteComment({ commentId: answer.commentId });
     return NextResponse.json({ newAnswer }, { status: 200 });
 };
--- src/server/services/comment.service.ts
+++ src/server/services/comment.service.ts
@@ -1,8 +1,11 @@
+import { CommentModel } from "../models/comment.model";
 
 export const getComment = async ({ commentId }: { commentId: string }) => {
-    //
+    const comment = await CommentModel.findById(commentId);
+    if (!comment) throw new Error("Comment not found");
+    return comment;
 };
 
 export const deleteComment = async ({ commentId }: { commentId: string }) => {
-    //
+    await CommentModel.findByIdAndDelete(commentId);
 };
