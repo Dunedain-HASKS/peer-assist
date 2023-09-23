61ca03052957b475591f583fa3d9a471f4a2394a
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-09-23 23:12:44 +0530

    complete ans card

--- src/components/AnswerCard.tsx
+++ src/components/AnswerCard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Typography, Box, IconButton, Avatar, Button, TextField } from '@mui/material'; // Assuming you are using Material-UI
+import { Container, Typography, Box, IconButton, Avatar, Button, TextField, Divider } from '@mui/material'; // Assuming you are using Material-UI
 import { ArrowDownward, Comment } from '@mui/icons-material'; // Assuming you are using Material-UI icons
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import { Answer } from '@/types/answer.interface';
@@ -9,6 +9,7 @@ import { useAuth } from '@/context/session';
 import CommentCard from './CommentCard';
 import { useRouter } from 'next/navigation';
 import SendIcon from '@mui/icons-material/Send';
+import { Grid } from '@mui/material';
 
 
 const AnswerCard = ({ id }: { id: string }) => {
@@ -29,70 +30,83 @@ const AnswerCard = ({ id }: { id: string }) => {
     if (!answer) return <div>Loading...</div>;
 
     return (
-        <Container sx={{
+        <Grid container sx={{
             color: '#333',
             padding: '20px',
         }}>
-            <Typography variant='h6'>{answer.content}</Typography>
-
-            <Box sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
-                <UserCard id={answer.user as string} />
-                <Typography variant='body2' sx={{ ml: 1, color: 'blue' }}>
-                    {new Date(answer.time).toLocaleString()}
-                </Typography>
-            </Box>
-
-            <Box sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
-                <IconButton sx={{ color: 'green' }} onClick={() => {
-                    upvoteAnswer({ answerId: id, session }).then((res) => {
-                        router.refresh();
-                    });
-                }}>
-                    <ArrowUpwardIcon fontSize='medium' />
-                </IconButton>
-                <Typography variant='body2'>{answer.upvotes.length - answer.downvotes.length}</Typography>
-                <IconButton sx={{ color: 'red' }} onClick={() => {
-                    downvoteAnswer({ answerId: id, session }).then((res) => {
-                        router.refresh();
-                    });
-                }}>
-                    <ArrowDownward fontSize='medium' />
-                </IconButton>
-
-                <div style={{ display: 'flex' }}>
-                    <TextField
-                        id="outlined-multiline-static"
-                        label="Add a comment"
-                        fullWidth
-                        value={comment}
-                        onChange={(e) => setComment(e.target.value)}
-                        InputLabelProps={{ style: { color: '#0E131F' } }}
-                        sx={{ mb: 1, mx: 1 }}
-                    />
-                    <Button variant="contained" sx={{ mb: 1, mx: 1 }}
-                        onClick={() => {
-                            postComment({
-                                answerId: id,
-                                comment_input: {
-                                    content: comment
-                                },
-                                session
-                            }).then((res) => {
-                                router.refresh();
-                            })
-                        }}
-                    ><SendIcon />
+            <div style={{ display: 'flex' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                    <Button variant="contained" sx={{ mb: 1, mx: 1 }} disabled={status === 'upvote'} onClick={() => {
+                        upvoteAnswer({ answerId: id, session }).then((res) => {
+                            router.refresh();
+                        });
+                    }}>
+                        <ArrowUpwardIcon fontSize='medium' />
+                    </Button>
+                    <Typography variant='h6' sx={{ textAlign: 'center', mb: 1 }}>{answer.upvotes.length - answer.downvotes.length}</Typography>
+                    <Button variant="contained" sx={{ mb: 1, mx: 1 }} disabled={status === 'downvote'} onClick={() => {
+                        downvoteAnswer({ answerId: id, session }).then((res) => {
+                            router.refresh();
+                        });
+                    }}>
+                        <ArrowDownward fontSize='medium' />
                     </Button>
                 </div>
-                <Typography variant='body2'>{answer.comments.length} Comments </Typography>
-                {answer.comments.map((id) => (
-                    <CommentCard key={String(id)} id={String(id)} />
-                ))}
-            </Box>
+                <Typography variant='h6'>{answer.content}</Typography>
+            </div>
 
-            <hr />
+            <div style={{width:'100%', display:'flex', flexDirection:'column', justifyContent:'flex-end', marginLeft:15}}>
+                <div style={{ display: 'flex', flexDirection: 'column', marginTop: 2, marginBottom:3 }}>
+                    <Typography variant='body2' sx={{ color: 'blue', ml: 'auto' }}>
+                        {new Date(answer.time).toLocaleString()}
+                    </Typography>
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+                        <UserCard id={answer.user as string} />
+                    </div>
+                </div>
+                <div style={{ minWidth: 80, display: 'flex', justifyContent: 'center', flexDirection: 'column', marginRight: '20px' }}>
 
-        </Container>
+
+                    <div style={{ display: 'flex' }}>
+                        <TextField
+                            id="outlined-multiline-static"
+                            label="Add a comment"
+                            fullWidth
+                            value={comment}
+                            onChange={(e) => setComment(e.target.value)}
+                            InputLabelProps={{ style: { color: '#0E131F' } }}
+                            sx={{ mb: 1, mx: 1 }}
+                        />
+                        <Button variant="contained" sx={{ mb: 1, mx: 1 }}
+                            onClick={() => {
+                                postComment({
+                                    answerId: id,
+                                    comment_input: {
+                                        content: comment
+                                    },
+                                    session
+                                }).then((res) => {
+                                    router.refresh();
+                                })
+                            }}
+                        ><SendIcon />
+                        </Button>
+                    </div>
+
+                    <Typography variant='body2'>{answer.comments.length} Comments </Typography>
+
+                    <div style={{ display: 'flex', flexDirection: 'column', marginTop: '5' }}>
+                        {answer.comments.map((id) => (
+                            <CommentCard key={String(id)} id={String(id)} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+
+            <Divider />
+
+        </Grid>
     );
 };
 
