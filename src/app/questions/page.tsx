2883c0b42a01fdc12377124014e3d736c31f4abb
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-09-23 06:06:42 +0530

    make questions page

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -6,59 +6,54 @@ import QuestionCard from "../../components/questionCard";
 
 const data = [
     {
-        user: 'Alice',
-        question: 'What inspired you to pursue your career in software engineering, and how has your journey been so far?',
-        answers: {
-            answer1: `I've always been fascinated by technology, and I decided to pursue a career in software engineering to be part of creating innovative solutions. My journey has been challenging yet rewarding. I've learned a lot and had the opportunity to work on exciting projects.`,
-            answer2: `My interest in software engineering started during my college days when I took a programming course. Since then, I've been passionate about it. My journey has been a rollercoaster with many learning experiences, but I'm enjoying every bit of it.`,
-            answer3: `I come from a family of engineers, so I was exposed to technology early on. This inspired me to pursue a career in software engineering. The journey has been a mix of hard work and exploration, but I'm grateful for the opportunities it has brought.`
-        }
+        user: 'User1',
+        question: 'How do you test a React component?',
+        body: 'I am having trouble testing my React components. Can someone provide some guidance?',
+        tags: ['React', 'Testing'],
+        upvotes: 10,
+        downvotes: 2,
+        comments: [
+            'You can use libraries like Jest and React Testing Library for testing React components.',
+            'Here is an example of a test for a simple React component:',
+        ],
+        answers: [
+            'First, install the necessary testing libraries using npm or yarn:',
+            'Then, write test cases for your components using Jest:',
+        ],
     },
     {
-        user: 'Bob',
-        question: 'If you could travel to any place in the world, where would you go and why?',
-        answers: {
-            answer1: `I've always dreamt of visiting the beautiful landscapes of New Zealand. The country's natural beauty and diverse geography are simply breathtaking, and I'd love to explore it.`,
-            answer2: `Japan is on the top of my travel list. I'm fascinated by its rich culture, history, and modern technology. Experiencing the blend of tradition and innovation would be incredible.`,
-            answer3: `I'd choose Italy. The art, architecture, and cuisine are captivating. Exploring the historic cities and indulging in delicious food would make for an unforgettable trip.`,
-        }
+        user: 'User2',
+        question: 'What are some best practices for responsive web design?',
+        body: 'I want to make my website responsive to different screen sizes. Any tips for best practices in responsive web design?',
+        tags: ['Web Design', 'Responsive Design'],
+        upvotes: 8,
+        downvotes: 1,
+        comments: [
+            'Responsive design is crucial for a great user experience. Some best practices include:',
+            'You should also consider using CSS media queries to adjust the layout based on screen size:',
+        ],
+        answers: [
+            '1. Use a mobile-first approach',
+            '2. Use CSS flexbox or grid for layout',
+        ],
     },
     {
-        user: 'Charlie',
-        question: 'What advice would you give to someone just starting their career in your field?',
-        answers: {
-            answer1: 'My advice would be to never stop learning. The tech industry evolves rapidly, so staying updated with new technologies and trends is crucial. Also, don\'t hesitate to ask for help and collaborate with others.',
-            answer2: `Building a strong network is essential. Connect with professionals in your field, attend conferences, and join online communities. It's a great way to learn, grow, and find opportunities.`,
-            answer3: 'Embrace challenges and setbacks as opportunities for growth. Don\'t be afraid to take on ambitious projects. The experience gained from tackling difficult tasks is invaluable in your career.'
-        }
+        user: 'User3',
+        question: 'How can I optimize my website for performance?',
+        body: 'My website is loading slowly. What are some techniques to improve website performance?',
+        tags: ['Web Performance', 'Optimization'],
+        upvotes: 15,
+        downvotes: 3,
+        comments: [
+            'Website performance optimization is essential for user satisfaction. Here are some tips:',
+            'You should also consider using a content delivery network (CDN) to serve assets more efficiently:',
+        ],
+        answers: [
+            '1. Minimize and compress JavaScript and CSS files',
+            '2. Optimize images and use lazy loading',
+        ],
     },
-    {
-        user: 'Alice',
-        question: 'What inspired you to pursue your career in software engineering, and how has your journey been so far?',
-        answers: {
-            answer1: `I've always been fascinated by technology, and I decided to pursue a career in software engineering to be part of creating innovative solutions. My journey has been challenging yet rewarding. I've learned a lot and had the opportunity to work on exciting projects.`,
-            answer2: `My interest in software engineering started during my college days when I took a programming course. Since then, I've been passionate about it. My journey has been a rollercoaster with many learning experiences, but I'm enjoying every bit of it.`,
-            answer3: `I come from a family of engineers, so I was exposed to technology early on. This inspired me to pursue a career in software engineering. The journey has been a mix of hard work and exploration, but I'm grateful for the opportunities it has brought.`
-        }
-    },
-    {
-        user: 'Bob',
-        question: 'If you could travel to any place in the world, where would you go and why?',
-        answers: {
-            answer1: `I've always dreamt of visiting the beautiful landscapes of New Zealand. The country's natural beauty and diverse geography are simply breathtaking, and I'd love to explore it.`,
-            answer2: `Japan is on the top of my travel list. I'm fascinated by its rich culture, history, and modern technology. Experiencing the blend of tradition and innovation would be incredible.`,
-            answer3: `I'd choose Italy. The art, architecture, and cuisine are captivating. Exploring the historic cities and indulging in delicious food would make for an unforgettable trip.`,
-        }
-    },
-    {
-        user: 'Charlie',
-        question: 'What advice would you give to someone just starting their career in your field?',
-        answers: {
-            answer1: 'My advice would be to never stop learning. The tech industry evolves rapidly, so staying updated with new technologies and trends is crucial. Also, don\'t hesitate to ask for help and collaborate with others.',
-            answer2: `Building a strong network is essential. Connect with professionals in your field, attend conferences, and join online communities. It's a great way to learn, grow, and find opportunities.`,
-            answer3: 'Embrace challenges and setbacks as opportunities for growth. Don\'t be afraid to take on ambitious projects. The experience gained from tackling difficult tasks is invaluable in your career.'
-        }
-    }
+    // Add more test data as needed
 ];
 
 //   export default data;
@@ -67,7 +62,7 @@ const data = [
 const Page: React.FC = () => {
     return (
         <Container>
-            <Typography variant="h2" sx={{ mt: 2, textAlign: 'center', color:'#261f49' }}>
+            <Typography variant="h2" sx={{ mt: 2, textAlign: 'center', color: '#261f49' }}>
                 Questions
             </Typography>
             {data.map((item, index) => (
