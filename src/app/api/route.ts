b5d4a91822e442598c315dc86297bb02268a8f82
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-23 11:52:36 +0530

    rename create to post

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import { closeQuestion, getQuestionThread, postCommentToQuestion, voteQuestion } from "@/server/services/question.service";
+import { closeQuestion, getQuestionThread, postCommentToQuestion, postQuestion, voteQuestion } from "@/server/services/question.service";
 
 export async function POST(request: NextRequest, context: any) {
-  
+    const { question, userId } = await request.json();
+    await postQuestion({ question_input: question, userId });
 }; 
