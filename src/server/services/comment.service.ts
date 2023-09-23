0a39ae189529a71a2e39f3506e20f0ffa3fd124a
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-23 18:34:59 +0530

    update services

--- src/server/services/comment.service.ts
+++ src/server/services/comment.service.ts
@@ -3,7 +3,10 @@ import { CommentModel } from "../models/comment.model";
 export const getComment = async ({ commentId }: { commentId: string }) => {
     const comment = await CommentModel.findById(commentId);
     if (!comment) throw new Error("Comment not found");
-    return comment;
+    return {
+        ...comment.toJSON(),
+        user: comment.user.toString(),
+    };
 };
 
 export const deleteComment = async ({ commentId }: { commentId: string }) => {
