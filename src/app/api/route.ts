8e2084f369bbdee7cd0e96d566972079715d48ab
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-23 19:37:04 +0530

    extend user dto

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,5 +2,5 @@ import { generateData } from "@/server/seeds/seed";
 import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest, context: any) {
     await generateData();
-    return NextResponse.redirect("/");
+    return NextResponse.json({ message: "Data generated" }, { status: 200, statusText: "OK"});
 }; 
