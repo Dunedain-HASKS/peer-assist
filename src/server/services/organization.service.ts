99c4ef4d05336c65f1ec1961351baf305899a91b
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-23 11:44:16 +0530

    resolve members error

--- src/server/services/organization.service.ts
+++ src/server/services/organization.service.ts
@@ -1,10 +1,19 @@
 import { OrganizationModel } from "../models/organization.model";
+import { UserModel } from "../models/user.model";
 
 
-export const getOrganization = async ({ organizationId }: { organizationId: string }) => {
+export const getOrganization = async ({ userId }: { userId: string }) => {
+    const user = await UserModel.findById(userId, {
+        organization: 1,
+    }).exec();
+    if (!user) throw new Error("User not found");
+    const organizationId = user.organization.toString();
     const organization = await OrganizationModel.findById(organizationId);
     if (!organization) throw new Error("Organization not found");
-    return organization;
+    return {
+        ...organization.toJSON(),
+        users: organization.users.map((user) => user.toString()),
+    }
 };
 
 export const upsertOrganization = async ({ domain }: { domain: string }) => {
