89611f95b65e9095338754cc645c0527c47728a1
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-23 11:51:53 +0530

    add users query action

--- src/app/users/action.ts
+++ src/app/users/action.ts
@@ -2,7 +2,7 @@
 
 import { getUsers } from "@/server/services/user.service";
 
-export async function fetchUser(query: string) {
+export async function fetchUsers({ query }: { query: string }) {
     const users = await getUsers({ query });
-    return users;
+    return { users };
 };
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -2,50 +2,46 @@
 
 import { Container, TextField } from "@mui/material";
 import Button from '@mui/material/Button';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import UserCard from '@/components/UserCard';
+import { fetchUsers } from "./action";
 
-const data = [
-    {
-        username: 'user1',
-        organization: 'org1'
-    },
-    {
-        username: 'user2',
-        organization: 'org1'
-    },
-    {
-        username: 'user3',
-        organization: 'org2'
-    },
-    {
-        username: 'user4',
-        organization: 'org3'
-    },
-];
 
 export default function Page() {
     const [searchQuery, setSearchQuery] = useState('');
-
+    const [data, setData] = useState<string[]>([]);
+    const [loading, setLoading] = useState(false);
+    
+    useEffect(() => {
+        fetchUsers({ query: ""}).then((data) => {
+            data?.users && setData(data.users);
+        });
+    }, []);
+    
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchQuery(event.target.value);
     };
 
     const handleSearch = () => {
-        console.log('searching')
+        setLoading(true);
+        fetchUsers({ query: searchQuery }).then((data) => {
+            data?.users && setData(data.users);
+            setLoading(false);
+        });
     }
 
     return (
         <>
-            <Container maxWidth="md" sx={{ }}>
-                <TextField  type="search" id="search" label="Search" sx={{ width: 600 }} onChange={handleInputChange} />
+            <Container maxWidth="md" sx={{}}>
+                <TextField type="search" id="search" label="Search" sx={{ width: 600 }} onChange={handleInputChange} />
                 <Button variant="contained" onClick={handleSearch}>
                     Search
                 </Button>
             </Container>
             <h1>Users</h1>
-            {data.map((item, index) => (
-                <UserCard key={index} id={item} />
+            {loading && <div>Loading...</div>}
+            {data.map((id) => (
+                <UserCard key={id} id={id} />
             ))}
         </>
     );
