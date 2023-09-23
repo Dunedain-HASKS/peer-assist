5c7dfd8c85b2cdec187a2d83897ed90a9d395021
Anuj Contractor <63806427+anujcontractor@users.noreply.github.com>
2023-09-23 18:16:21 +0530

    update usercard

--- src/app/organization/page.tsx
+++ src/app/organization/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Container, Typography } from "@mui/material";
+import { Container, Divider, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { fetchOrganization } from "./action";
 import { useAuth } from "@/context/session";
@@ -18,18 +18,19 @@ export default function Page() {
     if (!organization) return <div>Loading...</div>;
     return (
         <>
-            <Typography variant="h2" sx={{ py: 1, color: "red" }}>Name: {organization.name}</Typography>
-            <Typography variant="h4" sx={{ py: 1, color: "red" }}>Domain: {organization.domain}</Typography>
-            <Typography variant="h5" sx={{ py: 1, color: "red" }}>Description: {organization.description}</Typography>
-            <ul style={{ paddingBottom: 5, fontSize: '1.5rem' }}>
+            <Typography variant="h2" sx={{ textAlign:'center', py: 1, color: "red" }}> {organization.name}</Typography>
+            <Typography variant="h4" sx={{ py: 1, color: "red",textAlign:'center' }}> {organization.domain}</Typography>
+            <Typography variant="h5" sx={{ py: 1, color: "red",textAlign:'center' }}> {organization.description}</Typography>
+            <Divider sx={{mb:2}}/>
+            <div style={{ paddingBottom: 5, fontSize: '1.5rem' }}>
                 {
                     organization.users.map((user) => (
-                        <li key={user as string}>
+                        <Typography key={user as string}>
                             <UserCard id={user as string} />
-                        </li>
+                        </Typography>
                     ))
                 }
-            </ul>
+            </div>
         </>
     );
 }
