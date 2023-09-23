26dd064e9c96d3af98096076293345dbdae7b95c
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-24 00:20:05 +0530

    add ask errors

--- src/app/ask/page.tsx
+++ src/app/ask/page.tsx
@@ -13,129 +13,132 @@ import MDEditor from "@uiw/react-md-editor";
 import { Divider } from "@mui/material";
 
 export default function Page() {
-  const router = useRouter();
-  const [selected, setSelected] = useState(["open"]);
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [loading, setLoading] = useState(false);
-  const { session } = useAuth();
-  const handleSubmit = (e: any) => {
-    setLoading(true);
-    e.preventDefault();
-    postQuestion({
-      question_input: {
-        title: title,
-        body: body,
-        tags: selected,
-      },
-      session: session,
-    }).then(({ questionId }) => {
-      setLoading(false);
-      router.push(`/questions/${questionId}`);
-    });
-  };
+    const router = useRouter();
+    const [selected, setSelected] = useState(["open"]);
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [loading, setLoading] = useState(false);
+    const { session } = useAuth();
+    const [error, setError] = useState("");
+    const handleSubmit = (e: any) => {
+        setLoading(true);
+        e.preventDefault();
+        postQuestion({
+            question_input: {
+                title: title,
+                body: body,
+                tags: selected,
+            },
+            session: session,
+        }).then(({ questionId, message }) => {
+            setLoading(false);
+            if (!questionId)
+                return setError(message);
+            else
+                router.push(`/questions/${questionId}`);
+        });
+    };
 
-  return (
-    <Paper
-      elevation={3}
-      sx={{
-        p: 4,
-        maxWidth: "100%",
-        margin: "auto",
-        bgcolor: "#eff1fe",
-        minHeight: "100vh",
-      }}
-    >
-      <Typography variant="h2" align="center" gutterBottom>
-        Ask
-      </Typography>
-        <Divider />
-      <form onSubmit={handleSubmit}>
-        <Typography variant="h5" gutterBottom sx={{ mt: 4 }}>
-          Title
-        </Typography>
-        <TextField
-          id="outlined-multiline-flexible"
-          placeholder="Title of your question"
-          label=""
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          InputProps={{ sx: { fontSize: "1.5rem", p: 1 } }}
-          InputLabelProps={{
-            sx: { fontSize: "1.5rem" },
-            style: { color: "#0E131F" },
-          }}
-          sx={{ minHeight: "5rem", color: "whitesmoke", marginBottom: "1rem" }}
-        />
-        <Typography variant="h5" gutterBottom sx={{ mt: 2 }}>
-          Question you want to ask
-        </Typography>
-        <TextField
-          id="outlined-multiline-static"
-          placeholder="Introduce the problem and expand on what you put in the title. Be specific and imagine you’re asking a question to another person."
-          label=""
-          multiline
-          rows={10}
-          fullWidth
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-          variant="outlined"
-          margin="normal"
-          InputProps={{ sx: { fontSize: "1rem", p: 4 } }}
-          InputLabelProps={{
-            sx: { fontSize: "1.5rem" },
-            style: { color: "#0E131F" },
-          }}
-          sx={{ minHeight: "5rem", color: "whitesmoke", marginBottom: "1rem" }}
-        />
-        <Typography variant="h5" gutterBottom sx={{ mt: 2 }}>
-          Preview
-        </Typography>
-        <div className="container" data-color-mode="light">
-          <MDEditor.Markdown
-            source={body}
-            style={{
-              fontSize: "15px",
-              display: "block",
-              margin: "auto auto",
-              padding: "2rem",
-              border: "1px solid #000000",
-              borderRadius: "4px",
+    return (
+        <Paper
+            elevation={3}
+            sx={{
+                p: 4,
+                maxWidth: "100%",
+                margin: "auto",
+                bgcolor: "#eff1fe",
+                minHeight: "100vh",
             }}
-          />
-        </div>
-        <Typography variant="h5" gutterBottom sx={{ mt: 4 }}>
-          Tags
-        </Typography>
-        <div style={{ backgroundColor: "#eff1fe" }}>
-          <TagsInput
-            value={selected}
-            onChange={setSelected}
-            name="tags"
-            placeHolder="tags"
-          />
-        </div>
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          sx={{
-            my: 2,
-            fontSize: "1.2rem",
-            width: "10vw",
-            color: "whitesmoke",
-            display: "block", // Make the button a block element
-            mt: 5, // Add top margin
-            ml: "auto", // Center horizontally
-            mr: "auto", // Center horizontally
-          }}
         >
-          Submit
-        </Button>
-      </form>
-    </Paper>
-  );
+            <Typography variant="h2" align="center" gutterBottom>
+                Ask
+            </Typography>
+            <Divider />
+            <form onSubmit={handleSubmit}>
+                <Typography variant="h5" gutterBottom sx={{ mt: 4 }}>
+                    Title
+                </Typography>
+                <TextField
+                    id="outlined-multiline-flexible"
+                    placeholder="Title of your question"
+                    label=""
+                    fullWidth
+                    variant="outlined"
+                    margin="normal"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    InputProps={{ sx: { fontSize: "1.5rem", p: 1 } }}
+                    InputLabelProps={{
+                        sx: { fontSize: "1.5rem" },
+                        style: { color: "#0E131F" },
+                    }}
+                    sx={{ minHeight: "5rem", color: "whitesmoke", marginBottom: "1rem" }}
+                />
+                <Typography variant="h5" gutterBottom sx={{ mt: 2 }}>
+                    Question you want to ask
+                </Typography>
+                <TextField
+                    id="outlined-multiline-static"
+                    placeholder="Introduce the problem and expand on what you put in the title. Be specific and imagine you’re asking a question to another person."
+                    label=""
+                    multiline
+                    rows={10}
+                    fullWidth
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                    variant="outlined"
+                    margin="normal"
+                    InputProps={{ sx: { fontSize: "1rem", p: 4 } }}
+                    InputLabelProps={{
+                        sx: { fontSize: "1.5rem" },
+                        style: { color: "#0E131F" },
+                    }}
+                    sx={{ minHeight: "5rem", color: "whitesmoke", marginBottom: "1rem" }}
+                />
+                <Typography variant="h5" gutterBottom sx={{ mt: 2 }}>
+                    Preview
+                </Typography>
+                <div className="container" data-color-mode="light">
+                    <MDEditor.Markdown
+                        source={body}
+                        style={{
+                            fontSize: "15px",
+                            display: "block",
+                            margin: "auto auto",
+                            padding: "2rem",
+                            border: "1px solid #000000",
+                            borderRadius: "4px",
+                        }}
+                    />
+                </div>
+                <Typography variant="h5" gutterBottom sx={{ mt: 4 }}>
+                    Tags
+                </Typography>
+                <div style={{ backgroundColor: "#eff1fe" }}>
+                    <TagsInput
+                        value={selected}
+                        onChange={setSelected}
+                        name="tags"
+                        placeHolder="tags"
+                    />
+                </div>
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    sx={{
+                        my: 2,
+                        fontSize: "1.2rem",
+                        width: "10vw",
+                        color: "whitesmoke",
+                        display: "block", // Make the button a block element
+                        ml: "auto", // Center horizontally
+                        mr: "auto", // Center horizontally
+                    }}
+                >
+                    Submit
+                </Button>
+            </form>
+        </Paper>
+    );
 }
