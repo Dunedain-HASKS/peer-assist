7cd3bf2d9436521fb3b557ad52b0a2483bdeab17
Anuj Contractor <63806427+anujcontractor@users.noreply.github.com>
2023-09-23 23:28:15 +0530

    make usercard and link it

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import { Container, Divider, Typography } from "@mui/material";
+import { UserBasic } from '@/types/user.interface';
+import { fetchUser } from './action';
+import Link from 'next/link';
+
+const UserCard = ({ id }: { id: string }) => {
+    const [user, setUser] = useState<UserBasic>();
+    useEffect(() => {
+        fetchUser({ userId: id }).then((data) => {
+            setUser(data?.user);
+            console.log("user", data?.user);
+        });
+    }, [id]);
+
+    if (!user) return <div>Loading...</div>;
+
+    const { username, organization } = user;
+
+    return (
+        <>  
+            <Link href={`/users/${id}`} style={{textDecoration: "none", marginLeft:'auto'}}>
+                <Typography variant="h6" sx={{ py: 0, ml:'auto', fontSize: "medium" }}>{username}</Typography>
+            </Link>
+                <Typography variant="h6" sx={{ py: 0, ml:'auto' }}>{organization.name}</Typography>
+        </>
+    );
+};
+
+export default UserCard;
