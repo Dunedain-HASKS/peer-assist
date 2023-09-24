7db16dfffcca5d8a36a03652c9099fe2f2178469
Harshal Patel <harshal5820@gmail.com>
2023-09-24 16:40:16 +0530

    add markdown editor with preview

--- src/app/ask/page.tsx
+++ src/app/ask/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TagsInput } from "react-tag-input-component";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
@@ -14,143 +14,134 @@ import MDEditor from "@uiw/react-md-editor";
 import { Divider } from "@mui/material";
 
 export default function Page() {
-    const router = useRouter();
-    const [selected, setSelected] = useState(["open"]);
-    const [title, setTitle] = useState("");
-    const [body, setBody] = useState("");
-    const [loading, setLoading] = useState(false);
-    const { session } = useAuth();
-    const [error, setError] = useState("");
-    const handleSubmit = (e: any) => {
-        setLoading(true);
-        e.preventDefault();
-        postQuestion({
-            question_input: {
-                title: title,
-                body: body,
-                tags: selected,
-            },
-            session: session,
-        }).then(({ questionId, message }) => {
-            setLoading(false);
-            if (!questionId)
-                return setError(message);
-            else
-                router.push(`/questions/${questionId}`);
-        });
-    };
+  const router = useRouter();
+  const [selected, setSelected] = useState(["open"]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { session } = useAuth();
+  const [error, setError] = useState("");
 
-    if (loading)
-    {
-       return( <Grid container component="main" maxWidth="xs" sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", padding: "40px", minHeight: "83vh" }}>
-                <div style={{ justifyContent: "center", alignItems: "center", display: "flex" }}>
-                    <Typography variant="h4" sx={{ marginLeft: "10px" }}>
-                        Posting your question...
-                    </Typography>
-                </div>
-            </Grid>)
-    }
+  const handleSubmit = (e: any) => {
+    setLoading(true);
+    e.preventDefault();
+    postQuestion({
+      question_input: {
+        title: title,
+        body: body,
+        tags: selected,
+      },
+      session: session,
+    }).then(({ questionId, message }) => {
+      setLoading(false);
+      if (!questionId) return setError(message);
+      else router.push(`/questions/${questionId}`);
+    });
+  };
 
+  if (loading) {
     return (
-        <Paper
-            elevation={3}
-            sx={{
-                p: 4,
-                maxWidth: "100%",
-                margin: "auto",
-                bgcolor: "#eff1fe",
-                minHeight: "100vh",
-            }}
+      <Grid
+        container
+        component="main"
+        maxWidth="xs"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "40px",
+          minHeight: "83vh",
+        }}
+      >
+        <div
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+          }}
         >
-            <Typography variant="h2" align="center" gutterBottom>
-                Ask
-            </Typography>
-            <Divider />
-            <form onSubmit={handleSubmit}>
-                <Typography variant="h5" gutterBottom sx={{ mt: 4 }}>
-                    Title
-                </Typography>
-                <TextField
-                    id="outlined-multiline-flexible"
-                    placeholder="Title of your question"
-                    label=""
-                    fullWidth
-                    variant="outlined"
-                    margin="normal"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    InputProps={{ sx: { fontSize: "1.5rem", p: 1 } }}
-                    InputLabelProps={{
-                        sx: { fontSize: "1.5rem" },
-                        style: { color: "#0E131F" },
-                    }}
-                    sx={{ minHeight: "5rem", color: "whitesmoke", marginBottom: "1rem" }}
-                />
-                <Typography variant="h5" gutterBottom sx={{ mt: 2 }}>
-                    Question you want to ask
-                </Typography>
-                <TextField
-                    id="outlined-multiline-static"
-                    placeholder="Introduce the problem and expand on what you put in the title. Be specific and imagine you’re asking a question to another person."
-                    label=""
-                    multiline
-                    rows={10}
-                    fullWidth
-                    value={body}
-                    onChange={(e) => setBody(e.target.value)}
-                    variant="outlined"
-                    margin="normal"
-                    InputProps={{ sx: { fontSize: "1rem", p: 4 } }}
-                    InputLabelProps={{
-                        sx: { fontSize: "1.5rem" },
-                        style: { color: "#0E131F" },
-                    }}
-                    sx={{ minHeight: "5rem", color: "whitesmoke", marginBottom: "1rem" }}
-                />
-                <Typography variant="h5" gutterBottom sx={{ mt: 2 }}>
-                    Preview
-                </Typography>
-                <div className="container" data-color-mode="light">
-                    <MDEditor.Markdown
-                        source={body}
-                        style={{
-                            fontSize: "15px",
-                            display: "block",
-                            margin: "auto auto",
-                            padding: "2rem",
-                            border: "1px solid #000000",
-                            borderRadius: "4px",
-                        }}
-                    />
-                </div>
-                <Typography variant="h5" gutterBottom sx={{ mt: 4 }}>
-                    Tags
-                </Typography>
-                <div style={{ backgroundColor: "#eff1fe" }}>
-                    <TagsInput
-                        value={selected}
-                        onChange={setSelected}
-                        name="tags"
-                        placeHolder="tags"
-                    />
-                </div>
-                <Button
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                    sx={{
-                        my: 2,
-                        fontSize: "1.2rem",
-                        width: "10vw",
-                        color: "whitesmoke",
-                        display: "block", // Make the button a block element
-                        ml: "auto", // Center horizontally
-                        mr: "auto", // Center horizontally
-                    }}
-                >
-                    Submit
-                </Button>
-            </form>
-        </Paper>
+          <Typography variant="h4" sx={{ marginLeft: "10px" }}>
+            Posting your question...
+          </Typography>
+        </div>
+      </Grid>
     );
+  }
+
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        p: 4,
+        maxWidth: "100%",
+        margin: "auto",
+        bgcolor: "#eff1fe",
+        minHeight: "100vh",
+      }}
+    >
+      <Typography variant="h2" align="center" gutterBottom>
+        Ask
+      </Typography>
+      <Divider />
+      <form onSubmit={handleSubmit}>
+        <Typography variant="h5" gutterBottom sx={{ mt: 4 }}>
+          Title
+        </Typography>
+        <TextField
+          id="outlined-multiline-flexible"
+          placeholder="Title of your question"
+          label=""
+          fullWidth
+          variant="outlined"
+          margin="normal"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          InputProps={{ sx: { fontSize: "1.5rem", p: 1 } }}
+          InputLabelProps={{
+            sx: { fontSize: "1.5rem" },
+            style: { color: "#0E131F" },
+          }}
+          sx={{ minHeight: "5rem", color: "whitesmoke", marginBottom: "1rem" }}
+        />
+        <Typography variant="h5" gutterBottom sx={{ mt: 2 }}>
+          Question you want to ask
+        </Typography>
+        <div style={{ height: "auto" }} data-color-mode="light">
+          <MDEditor
+            height={450}
+            value={body}
+            onChange={(value) => setBody(value || "")}
+          />
+        </div>
+        <Typography variant="h5" gutterBottom sx={{ mt: 4 }}>
+          Tags
+        </Typography>
+        <div style={{ backgroundColor: "#eff1fe" }}>
+          <TagsInput
+            value={selected}
+            onChange={setSelected}
+            name="tags"
+            placeHolder="tags"
+          />
+        </div>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{
+            my: 2,
+            fontSize: "1.2rem",
+            width: "10vw",
+            color: "whitesmoke",
+            display: "block", // Make the button a block element
+            ml: "auto", // Center horizontally
+            mr: "auto", // Center horizontally
+          }}
+        >
+          Submit
+        </Button>
+      </form>
+    </Paper>
+  );
 }
--- src/app/questions/[id]/answer/page.tsx
+++ src/app/questions/[id]/answer/page.tsx
@@ -1,91 +1,103 @@
 "use client";
 
 import { useState } from "react";
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 import QuestionCard from "@/components/QuestionCard";
 import { useAuth } from "@/context/session";
 import { postAnswerAction } from "./action";
 import { useRouter } from "next/navigation";
+import MDEditor from "@uiw/react-md-editor";
 
 export default function Page({ params }: { params: { id: string } }) {
-    const [content, setBody] = useState("");
-    const [loading, setLoading] = useState(false);
-    const { session } = useAuth();
-    const [error, setError] = useState("");
-    const router = useRouter();
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
-        setLoading(true);
-        postAnswerAction({
-            answer_input: {
-                content
-            },
-            questionId: params.id,
-            session,
-        }).then((res) => {
-            setLoading(false);
-            if (!res.answer)
-                setError(res.message);
-            else
-                router.push(`/questions/${params.id}`);
-        });
-    }
+  const [content, setBody] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { session } = useAuth();
+  const [error, setError] = useState("");
+  const router = useRouter();
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    postAnswerAction({
+      answer_input: {
+        content,
+      },
+      questionId: params.id,
+      session,
+    }).then((res) => {
+      setLoading(false);
+      if (!res.answer) setError(res.message);
+      else router.push(`/questions/${params.id}`);
+    });
+  };
 
-    if (loading)
-    {
-        return (
-            <Grid container component="main" maxWidth="xs" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: '40px', minHeight: '83vh' }}>
-                <div style={{ justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
-                    <Typography variant="h4" sx={{ marginLeft: '10px' }}>
-                        Posting your answer...
-                    </Typography>
-                </div>
-            </Grid>
-        )
-    }
-
+  if (loading) {
     return (
-        <Paper elevation={3} sx={{ p: 2, maxWidth: '100%', margin: 'auto', bgcolor: '#eff1fe' }}>
-            <Typography variant="h2" align="center" gutterBottom>
-                Your Solution
-            </Typography>
-            <QuestionCard id={params.id} />
-            <form onSubmit={handleSubmit}>
-                <TextField
-                    id="outlined-multiline-static"
-                    label="Your Solution"
-                    multiline
-                    rows={4}
-                    fullWidth
-                    value={content}
-                    onChange={(e) => setBody(e.target.value)}
-                    variant="outlined"
-                    margin="normal"
-                    InputProps={{ sx: { fontSize: '1.5rem' }, }}
-                    InputLabelProps={{ sx: { fontSize: '1.5rem' }, style: { color: '#0E131F' } }}
-                    sx={{ minHeight: '5rem', color: "whitesmoke", marginBottom: "1rem" }} />
-                <Button
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                    sx={{
-                        my: 2,
-                        fontSize: '1.2rem',
-                        width: '10vw',
-                        color: 'whitesmoke',
-                        display: 'block',
-                        ml: 'auto',
-                        mr: 'auto',
-                    }}
-                >
-                    Submit
-                </Button>
+      <Grid
+        container
+        component="main"
+        maxWidth="xs"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "40px",
+          minHeight: "83vh",
+        }}
+      >
+        <div
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+          }}
+        >
+          <Typography variant="h4" sx={{ marginLeft: "10px" }}>
+            Posting your answer...
+          </Typography>
+        </div>
+      </Grid>
+    );
+  }
 
-            </form>
-        </Paper>
-    )
+  return (
+    <Paper
+      elevation={3}
+      sx={{ p: 2, maxWidth: "100%", margin: "auto", bgcolor: "#eff1fe" }}
+    >
+      <Typography variant="h2" align="center" gutterBottom>
+        Your Solution
+      </Typography>
+      <QuestionCard id={params.id} />
+      <form onSubmit={handleSubmit}>
+        <div style={{ height: "auto" }} data-color-mode="light">
+          <MDEditor
+            height={450}
+            value={content}
+            onChange={(value) => setBody(value || "")}
+          />
+        </div>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{
+            my: 2,
+            fontSize: "1.2rem",
+            width: "10vw",
+            color: "whitesmoke",
+            display: "block",
+            ml: "auto",
+            mr: "auto",
+          }}
+        >
+          Submit
+        </Button>
+      </form>
+    </Paper>
+  );
 }
