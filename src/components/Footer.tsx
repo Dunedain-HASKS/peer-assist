63cb12ef07c11a580c85458d4adb1ed711049718
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-09-24 14:43:07 +0530

    add github icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,15 +4,23 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-export default function Footer(){
-    return (
-        <AppBar sx={{ top: 'auto', bottom: 0, position:'inherit' }}>
-      <Toolbar>
-        <Typography variant="body1" color="inherit" sx={{ flexGrow: 1, textAlign: "center" }}>
+export default function Footer() {
+  const githubRepoLink = 'https://github.com/Dunedain-HASKS/peer-assist';
+
+  return (
+    <AppBar sx={{ top: 'auto', bottom: 0, position: 'inherit' }}>
+      <Toolbar sx={{display:'flex', justifyContent:'center'}}>
+        <Typography variant="body1" color="inherit" sx={{textAlign: 'center' }}>
           &copy; {new Date().getFullYear()} Peer Assist. All rights reserved.
         </Typography>
+        {/* Add the GitHub icon with a link */}
+        <IconButton color="inherit" href={githubRepoLink} target="_blank" rel="noopener noreferrer">
+          <GitHubIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
-    );
+  );
 }
